7e8fd8161153532e71aedf29b6634023ec1d2b0e
alaster <igor.aka92@gmail.com>
2022-12-15 14:06:28 +0200

    13

--- src/main/java/org/example/puzzle11/Puzzle11.java
+++ src/main/java/org/example/puzzle11/Puzzle11.java
@@ -3,7 +3,6 @@ package org.example.puzzle11;
 import org.apache.commons.lang3.StringUtils;
 import org.example.Utils;
 
-import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
@@ -11,22 +10,23 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
-import java.util.function.BinaryOperator;
-import java.util.function.IntBinaryOperator;
-import java.util.function.IntUnaryOperator;
-import java.util.function.UnaryOperator;
+import java.util.Set;
+import java.util.function.LongBinaryOperator;
+import java.util.function.LongUnaryOperator;
 import java.util.regex.MatchResult;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 public class Puzzle11 {
 //	static final int rounds = 20;
-//	static final BigInteger divisor = BigInteger.valueOf(3);
+//	static final int divisor = 3;
 	static final int rounds = 10000;
-	static final BigInteger divisor = BigInteger.ONE;
+	static final int divisor = 1;
+	static int testsMultiple = 1;
+	static final Set<Integer> checks = Set.of(1, 20, 1000, 2000);
 	
 	public static void main(String[] args) {
-		Scanner input = Utils.scanFileNearClass(Puzzle11.class, "example.txt");
+		Scanner input = Utils.scanFileNearClass(Puzzle11.class, "input.txt");
 		Pattern pattern = Pattern.compile("""
 				Monkey (\\d+):
 				  Starting items: (.+)
@@ -38,19 +38,21 @@ public class Puzzle11 {
 		Map<Integer, Integer> stats = new HashMap<>();
 		Game game = new Game(monkeys);
 		System.out.println(game);
-		
+		testsMultiple = game.testsMultiple();
 		
 		for (int round = 0; round < rounds; round++) {
-			if (round % 100 == 0) {
+			if (checks.contains(round)) {
 				System.out.println("Round: " + round);
+				System.out.println(game);
 				printStats(stats);
 			}
 			game.round(stats);
 		}
 		printStats(stats);
-		System.out.println("Answer: " + stats.values().stream()
+		System.out.println("Answer 1: " + stats.values().stream()
 				.sorted(Comparator.<Integer>naturalOrder().reversed())
 				.limit(2)
+				.mapToLong(Integer::longValue)
 				.reduce((left, right) -> left * right)
 				.orElse(0));
 	}
@@ -69,14 +71,16 @@ public class Puzzle11 {
 		}
 		
 		void turn(Monkey monkey, Map<Integer, Integer> stats) {
-			stats.compute(monkey.id(), (id, count) -> monkey.items().size() + (count == null ? 0 : count));
 			while (!monkey.items().isEmpty()) {
-				BigInteger item = monkey.items().remove(0);
-				item = monkey.operation().apply(item);
-//				if (divisor != BigInteger.ONE)
-//					item = item.divide(divisor);
+				long item = monkey.items().remove(0);
+				item = monkey.operation().applyAsLong(item) / divisor % testsMultiple;
 				monkeys.get(monkey.target(item)).items().add(item);
+				stats.compute(monkey.id(), (id, count) -> count == null ? 1 : count + 1);
 			}
+		}
+		
+		int testsMultiple() {
+			return monkeys().values().stream().mapToInt(Monkey::test).reduce((a, b) -> a * b).orElse(1);
 		}
 		
 		@Override
@@ -87,45 +91,42 @@ public class Puzzle11 {
 	
 	record Monkey(
 			int id,
-			List<BigInteger> items,
-			UnaryOperator<BigInteger> operation,
-			BigInteger test,
+			List<Long> items,
+			LongUnaryOperator operation,
+			int test,
 			int successTarget,
 			int failureTarget
 	) {
-		int target(BigInteger item) {
-			return item.mod(test).signum() == 0 ? successTarget : failureTarget;
+		int target(long item) {
+			return item % test == 0 ? successTarget : failureTarget;
 		}
 		
 		static Monkey parse(MatchResult matchResult) {
 			int id = Integer.parseInt(matchResult.group(1));
-			List<BigInteger> items = Arrays.stream(matchResult.group(2).split(", ")).map(BigInteger::new).toList();
-			UnaryOperator<BigInteger> operation = parseOperation(matchResult.group(3));
-			BigInteger test = new BigInteger(matchResult.group(4));
+			List<Long> items = Arrays.stream(matchResult.group(2).split(", ")).mapToLong(Long::parseLong).boxed().toList();
+			LongUnaryOperator operation = parseOperation(matchResult.group(3));
+			int test = Integer.parseInt(matchResult.group(4));
 			int successTarget = Integer.parseInt(matchResult.group(5));
 			int failureTarget = Integer.parseInt(matchResult.group(6));
 			return new Monkey(id, new ArrayList<>(items), operation, test, successTarget, failureTarget);
 		}
 		
-		static UnaryOperator<BigInteger> parseOperation(String group) {
+		static LongUnaryOperator parseOperation(String group) {
 			String[] split = StringUtils.removeStart(group, "new = ").split(" ");
-			BinaryOperator<BigInteger> operator = switch (split[1]) {
-				case "+" -> BigInteger::add;
-				case "-" -> BigInteger::subtract;
-				case "*" -> BigInteger::multiply;
-				case "/" -> BigInteger::divide;
+			LongBinaryOperator operator = switch (split[1]) {
+				case "+" -> (a, b) -> a + b;
+				case "-" -> (a, b) -> a - b;
+				case "*" -> (a, b) -> a * b;
+				case "/" -> (a, b) -> a / b;
 				default -> throw new RuntimeException("Unknown operator " + split[1]);
 			};
-			UnaryOperator<BigInteger> left = parseOperationValue(split[0]);
-			UnaryOperator<BigInteger> right = parseOperationValue(split[2]);
-			return old -> operator.apply(left.apply(old), right.apply(old));
+			LongUnaryOperator left = parseOperationValue(split[0]);
+			LongUnaryOperator right = parseOperationValue(split[2]);
+			return old -> operator.applyAsLong(left.applyAsLong(old), right.applyAsLong(old));
 		}
 		
-		static UnaryOperator<BigInteger> parseOperationValue(String value) {
-			if ("old".equals(value))
-				return old -> old;
-			BigInteger num = new BigInteger(value);
-			return old -> num;
+		static LongUnaryOperator parseOperationValue(String value) {
+			return "old".equals(value) ? old -> old : old -> Long.parseLong(value);
 		}
 	}
 }
---
+++ src/main/java/org/example/puzzle13/Puzzle13.java
@@ -0,0 +1,147 @@
+package org.example.puzzle13;
+
+import org.example.Utils;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Objects;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class Puzzle13 {
+	
+	public static void main1(String[] args) {
+		Scanner input = Utils.scanFileNearClass(Puzzle13.class, "input.txt");
+		
+		int result = 0;
+		int pair = 1;
+		while (input.hasNext()) {
+			Element left = Element.parse(input.nextLine());
+			Element right = Element.parse(input.nextLine());
+			int compare = left.compareTo(right);
+			System.out.println(pair + ": " + compare);
+			if (compare <= 0)
+				result += pair;
+			
+			if (input.hasNext())
+				input.nextLine();
+			pair++;
+		}
+		System.out.println("Answer 1: " + result);
+	}
+	
+	public static void main(String[] args) {
+		Scanner input = Utils.scanFileNearClass(Puzzle13.class, "input.txt");
+		
+		List<Element> elements = new ArrayList<>();
+		while (input.hasNext()) {
+			String line = input.nextLine();
+			if (!line.isEmpty())
+				elements.add(Element.parse(line));
+		}
+		Set<Element> dividers = Set.of(divider(2), divider(6));
+		
+		elements.addAll(dividers);
+		elements.sort(Comparator.naturalOrder());
+		
+		int result = 1;
+		int pair = 1;
+		for (Element element : elements) {
+			if (dividers.contains(element))
+				result *= pair;
+			pair++;
+		}
+		System.out.println("Answer 2: " + result);
+	}
+	
+	static ListElement divider(int value) {
+		return new ListElement(List.of(new ListElement(List.of(new NumberElement(value)))));
+	}
+	
+	sealed interface Element extends Comparable<Element> {
+		
+		static Element parse(String value) {
+			if (value.startsWith("["))
+				return ListElement.parse(value.substring(1, value.length() - 1));
+			return NumberElement.parse(value);
+		}
+	}
+	
+	record NumberElement(int value) implements Element {
+			@Override
+			public int compareTo(Element other) {
+				if (other instanceof NumberElement otherNumber)
+					return Integer.compare(value, otherNumber.value);
+				if (other instanceof ListElement otherList)
+					return - otherList.compareTo(new ListElement(List.of(this)));
+				throw new RuntimeException("Unknown type: " + other.getClass());
+			}
+			
+			@Override
+			public String toString() {
+				return "" + value;
+			}
+			
+			static Element parse(String value) {
+				return new NumberElement(Integer.parseInt(value));
+			}
+		}
+	
+	record ListElement(List<Element> children) implements Element {
+		
+		@Override
+		public int compareTo(Element other) {
+			if (other instanceof ListElement otherList) {
+				Iterator<Element> left = children.iterator();
+				Iterator<Element> right = otherList.children.iterator();
+				while (true) {
+					if (!left.hasNext())
+						return right.hasNext() ? -1 : 0;
+					if (!right.hasNext())
+						return 1;
+					int compare = left.next().compareTo(right.next());
+					if (compare != 0)
+						return compare;
+				}
+			} else if (other instanceof NumberElement otherNumber) {
+				return compareTo(new ListElement(List.of(otherNumber)));
+			} else {
+				throw new RuntimeException("Not supported for " + other.getClass());
+			}
+		}
+		
+		@Override
+		public String toString() {
+			return children.stream().map(Objects::toString).collect(Collectors.joining(", ", "[", "]"));
+		}
+		
+		static ListElement parse(String value) {
+			if (value.isEmpty())
+				return new ListElement(List.of());
+			
+			List<String> parts = new ArrayList<>();
+			int bracketsCount = 0;
+			int start = 0;
+			for (int i = 0; i < value.length(); i++) {
+				char c = value.charAt(i);
+				switch (c) {
+					case ',' -> {
+						if (bracketsCount == 0) {
+							parts.add(value.substring(start, i));
+							start = i + 1;
+						}
+					}
+					case '[' -> bracketsCount++;
+					case ']' -> bracketsCount--;
+					default -> {}
+				}
+			}
+			parts.add(value.substring(start));
+			List<Element> children = parts.stream().map(Element::parse).collect(Collectors.toList());
+			return new ListElement(children);
+		}
+	}
+}
