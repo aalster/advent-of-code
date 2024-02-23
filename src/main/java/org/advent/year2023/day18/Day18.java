13aea27132fc4aeab3a5c2cab99b358c6a6f1627
alaster <igor.aka92@gmail.com>
2024-02-23 17:00:21 +0200

    2023, day 18 part 2

--- src/main/java/org/advent/year2023/day18/Day18.java
+++ src/main/java/org/advent/year2023/day18/Day18.java
@@ -9,17 +9,12 @@ import org.apache.commons.lang3.StringUtils;
 
 import java.math.BigInteger;
 import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Comparator;
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
 import java.util.Set;
-import java.util.function.UnaryOperator;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class Day18 {
 	
@@ -39,7 +34,7 @@ public class Day18 {
 	}
 	
 	private static BigInteger part2(List<String> lines) {
-		return solveFast(lines.stream().map(Move::parse1).toList());
+		return solveFast(lines.stream().map(Move::parse2).toList());
 	}
 	
 	private static int solveNaive(List<Move> moves) {
@@ -72,186 +67,29 @@ public class Day18 {
 			inside.addAll(middle);
 		}
 
-//		Point.printField(Stream.of(path, inside).flatMap(Set::stream).collect(Collectors.toSet()), '#', '.');
 		return path.size() + inside.size();
 	}
 	
 	private static BigInteger solveFast(List<Move> moves) {
-		BigPoint current = new BigPoint(BigInteger.ZERO, BigInteger.ZERO);
-		Set<Line> lines = new HashSet<>();
-		for (Move move : moves) {
-			BigPoint next = current.shift(move.direction(), BigInteger.valueOf(move.meters()));
-			lines.add(Line.of(current, next));
-			current = next;
-		}
-		Shape shape = Shape.of(lines);
-//		shape.print();
-		BigInteger result = BigInteger.ZERO;
-		while (!shape.lines().isEmpty()) {
-			BigInteger area = shape.cutTop();
-			result = result.add(area);
-//			System.out.println("\nCut " + area + ": ");
-//			shape.print();
-		}
-		
-		return result;
-	}
-	
-	record Line(BigPoint start, BigPoint end, boolean horizontal) {
-		
-		Line shift(Direction direction, BigInteger amount) {
-			return new Line(start.shift(direction, amount), end.shift(direction, amount), horizontal);
-		}
-		
-		BigInteger length() {
-			return BigInteger.ONE.add(horizontal ? end.x().subtract(start.x()) : end.y().subtract(start.y()));
-		}
-		
-		BigInteger intersectionLength(Line l) {
-			if (l.horizontal != horizontal)
-				throw new RuntimeException("Not supported");
-			if (horizontal && start.y().compareTo(l.start.y()) == 0)
-				return min(end.x(), l.end.x()).subtract(max(start.x(), l.start.x())).add(BigInteger.ONE);
-			else if (!horizontal && start.x().compareTo(l.start.x()) == 0)
-				return min(end.y(), l.end.y()).subtract(max(start.y(), l.start.y())).add(BigInteger.ONE);
-			throw new RuntimeException("Not supported");
-		}
-		
-		Stream<BigPoint> containingPoints() {
-			return Stream.iterate(start, current -> !current.equals(end),
-					bigPoint -> bigPoint.shift(horizontal ? Direction.RIGHT : Direction.DOWN, BigInteger.ONE));
-		}
-		
-		Line combine(Line l) {
-			if (l.horizontal != horizontal)
-				throw new IllegalArgumentException("Different orientation");
-			List<BigPoint> points = Stream.of(start, end, l.start, l.end)
-					.collect(Collectors.toMap(p -> p, p -> 1, Integer::sum))
-					.entrySet().stream()
-					.filter(e -> e.getValue() == 1)
-					.map(Map.Entry::getKey)
-					.toList();
-			if (points.size() != 2)
-				throw new IllegalArgumentException("Lines don't touch");
-			return Line.of(points.getFirst(), points.getLast());
-		}
-		
-		static BigInteger lengthSum(Collection<Line> lines) {
-			return lines.stream().map(Line::length).reduce(BigInteger.ZERO, BigInteger::add);
-		}
-		
-		static Line of(BigPoint start, BigPoint end) {
-			if (start.equals(end))
-				throw new IllegalArgumentException("Zero line");
-			if (start.x().compareTo(end.x()) != 0 && start.y().compareTo(end.y()) != 0)
-				throw new IllegalArgumentException("Line must be vertical or horizontal");
-			
-			boolean horizontal = start.y().compareTo(end.y()) == 0;
-			if (start.x().add(start.y()).compareTo(end.x().add(end.y())) < 0)
-				return new Line(start, end, horizontal);
-			else
-				return new Line(end, start, horizontal);
-		}
-	}
-	
-	record Shape(Set<Line> lines, Map<BigPoint, List<Line>> edges) {
-		
-		Line otherLine(BigPoint edge, Line exclude) {
-			List<Line> list = edges.getOrDefault(edge, List.of()).stream().filter(l -> !l.equals(exclude)).toList();
-			if (list.size() != 1) {
-				System.out.println(edges.entrySet().stream()
-						.map(e -> e.getKey() + ": " + e.getValue())
-						.collect(Collectors.joining("\n")));
-				throw new RuntimeException("Bad edge " + edge + ": " + list);
+		// https://en.wikipedia.org/wiki/Shoelace_formula
+		List<BigPoint> edges = new ArrayList<>();
+		BigInteger perimeter = BigInteger.ZERO;
+		{
+			BigPoint current = new BigPoint(BigInteger.ZERO, BigInteger.ZERO);
+			for (Move move : moves) {
+				edges.add(current);
+				perimeter = perimeter.add(BigInteger.valueOf(move.meters()));
+				current = current.shift(move.direction(), BigInteger.valueOf(move.meters()));
 			}
-			return list.getFirst();
 		}
 		
-		BigInteger cutTop() {
-			Line top = lines.stream()
-					.filter(Line::horizontal)
-					.min(Comparator.comparing(l -> l.start().y()))
-					.orElseThrow(() -> new RuntimeException("Top not found"));
-			Line left = otherLine(top.start(), top);
-			Line right = otherLine(top.end(), top);
-			
-			if (left.end().y().compareTo(right.end().y()) == 0) {
-				Line nextLeft = otherLine(left.end(), left);
-				Line nextRight = otherLine(right.end(), right);
-				if (nextLeft.equals(nextRight)) {
-					replaceLines(List.of(), lines.stream().toList());
-					return top.length().multiply(left.length());
-				} else {
-					Line nextTop = top.shift(Direction.DOWN, left.length().subtract(BigInteger.ONE)).combine(nextLeft).combine(nextRight);
-					if (checkIntersection(nextTop, Set.of(top, left, right, nextLeft, nextRight))) {
-						rotateRight();
-						return BigInteger.ZERO;
-					}
-					replaceLines(List.of(nextTop), List.of(top, left, right, nextLeft, nextRight));
-					return top.length().multiply(left.length().subtract(BigInteger.ONE))
-							.add(top.shift(Direction.DOWN, left.length().subtract(BigInteger.ONE)).intersectionLength(nextLeft).subtract(BigInteger.ONE))
-							.add(top.shift(Direction.DOWN, right.length().subtract(BigInteger.ONE)).intersectionLength(nextRight).subtract(BigInteger.ONE));
-				}
-			}
-			Line min = left.end().y().compareTo(right.end().y()) < 0 ? left : right;
-			Line max = min == left ? right : left;
-			Line maxLeftover = Line.of(max.start().shift(Direction.DOWN, min.length().subtract(BigInteger.ONE)), max.end());
-			Line nextMin = otherLine(min.end(), min);
-			Line nextTop = top.shift(Direction.DOWN, min.length().subtract(BigInteger.ONE)).combine(nextMin);
-			if (checkIntersection(nextTop, Set.of(top, left, right, nextMin))) {
-				rotateRight();
-				return BigInteger.ZERO;
-			}
-			replaceLines(List.of(nextTop, maxLeftover), List.of(top, left, right, nextMin));
-			return top.length().multiply(min.length().subtract(BigInteger.ONE))
-					.add(top.shift(Direction.DOWN, min.length().subtract(BigInteger.ONE)).intersectionLength(nextMin).subtract(BigInteger.ONE));
+		BigInteger area = BigInteger.ZERO;
+		for (int i = 0; i < edges.size(); i++) {
+			BigPoint current = edges.get(i);
+			BigPoint next = edges.get((i + 1) % edges.size());
+			area = area.add(current.y().add(next.y()).multiply(current.x().subtract(next.x())));
 		}
-		
-		void replaceLines(List<Line> add, List<Line> remove) {
-			remove.forEach(lines::remove);
-			lines.addAll(add);
-			for (Line line : remove)
-				Stream.of(line.start(), line.end()).forEach(edge -> edges.get(edge).remove(line));
-			for (Line line : add)
-				Stream.of(line.start(), line.end()).forEach(edge -> edges.computeIfAbsent(edge, k -> new ArrayList<>()).add(line));
-		}
-		
-		boolean checkIntersection(Line nextTop, Set<Line> exclude) {
-			return lines.stream()
-					.filter(l -> !exclude.contains(l))
-					.flatMap(l -> Stream.of(l.start, l.end))
-					.filter(p -> p.y().compareTo(nextTop.start.y()) < 0)
-					.anyMatch(p -> nextTop.start.x().compareTo(p.x()) <= 0 && p.x().compareTo(nextTop.end.x()) <= 0);
-		}
-		
-		void rotateRight() {
-			UnaryOperator<BigPoint> pointRotation = p -> new BigPoint(p.y().negate(), p.x());
-			Set<Line> rotated = lines.stream().map(line -> Line.of(pointRotation.apply(line.start()), pointRotation.apply(line.end()))).collect(Collectors.toSet());
-			lines.clear();
-			lines.addAll(rotated);
-			edges.clear();
-			edges.putAll(edgesMap(rotated));
-		}
-		
-		BigInteger perimeter() {
-			return Line.lengthSum(lines).subtract(BigInteger.valueOf(lines.size()));
-		}
-		
-		void print() {
-			BigPoint.printField(lines.stream().flatMap(Line::containingPoints).collect(Collectors.toSet()), '#', '.');
-		}
-		
-		static Shape of(Set<Line> lines) {
-			return new Shape(new HashSet<>(lines), edgesMap(lines));
-		}
-		
-		private static Map<BigPoint, List<Line>> edgesMap(Set<Line> lines) {
-			Map<BigPoint, List<Line>> edges = new HashMap<>();
-			for (Line line : lines)
-				Stream.of(line.start(), line.end())
-						.forEach(p -> edges.computeIfAbsent(p, k -> new ArrayList<>()).add(line));
-			return edges;
-		}
+		return area.add(perimeter).divide(BigInteger.TWO).add(BigInteger.ONE);
 	}
 	
 	record Move(Direction direction, long meters) {
@@ -269,13 +107,5 @@ public class Day18 {
 			long value = Long.parseLong(hex, 16);
 			return new Move(directionsByIndex[(int) (value % 16)], value / 16);
 		}
-	}
-	
-	static BigInteger min(BigInteger l, BigInteger r) {
-		return l.compareTo(r) < 0 ? l : r;
-	}
-	
-	static BigInteger max(BigInteger l, BigInteger r) {
-		return l.compareTo(r) < 0 ? r : l;
 	}
 }
