691a4c2d23297e438a4dce36730ec7cb8667b315
alaster <igor.aka92@gmail.com>
2024-12-25 15:45:21 +0200

    2015, day 20

--- src/main/java/org/advent/year2015/day20/Day20.java
+++ src/main/java/org/advent/year2015/day20/Day20.java
@@ -10,29 +10,45 @@ public class Day20 {
 		Scanner input = Utils.scanFileNearClass(Day20.class, "input.txt");
 		int targetPresents = Integer.parseInt(input.nextLine());
 		
-		long start = System.currentTimeMillis();
 		System.out.println("Answer 1: " + part1(targetPresents));
-		System.out.println("Answer 2: " + part2());
-		System.out.println((System.currentTimeMillis() - start) + "ms");
+		System.out.println("Answer 2: " + part2(targetPresents));
 	}
 	
 	private static long part1(int targetPresents) {
-		targetPresents /= 10;
-		for (int i = 1; i < 100000; i++) {
-//			System.out.println(i + " - " + countPresents(i));
-			int presents = 0;
-			for (int i1 = 1; i1 <= i / 2; i1++) {
-				if (i % i1 == 0)
-					presents += i1;
-			}
-			if (presents + i >= targetPresents)
+		for (int i = 1; i < 1000000; i++)
+			if (countPresents1(i) >= targetPresents)
 				return i;
+		return 0;
+	}
+	
+	private static long part2(int targetPresents) {
+		for (int i = 1; i < 1000000; i++)
+			if (countPresents2(i) >= targetPresents)
+				return i;
+		return 0;
+	}
+	
+	private static int countPresents1(int house) {
+		int presents = 0;
+		double sqrt = Math.sqrt(house);
+		for (int i = 1; i <= sqrt; i++)
+			if (house % i == 0)
+				presents += i + house / i;
+		return presents * 10;
+	}
+	
+	private static int countPresents2(int house) {
+		int presents = 0;
+		double sqrt = Math.sqrt(house);
+		for (int i = 1; i <= sqrt; i++) {
+			if (house % i == 0) {
+				int divided = house / i;
+				if (divided < 50)
+					presents += i;
+				if (i < 50)
+					presents += divided;
+			}
 		}
-		return 0;
+		return presents * 11;
 	}
-	
-	private static long part2() {
-		return 0;
-	}
-	
 }
