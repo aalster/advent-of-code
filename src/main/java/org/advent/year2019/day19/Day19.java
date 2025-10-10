799a20d3fbb45d11e90ad8b0a2136776bd21d236
alaster <igor.aka92@gmail.com>
2025-10-11 00:54:55 +0300

    2019, day 19

---
+++ src/main/java/org/advent/year2019/day19/Day19.java
@@ -0,0 +1,61 @@
+package org.advent.year2019.day19;
+
+import org.advent.common.Point;
+import org.advent.common.Utils;
+import org.advent.runner.AdventDay;
+import org.advent.runner.DayRunner;
+import org.advent.runner.ExpectedAnswers;
+import org.advent.year2019.intcode_computer.InputProvider;
+import org.advent.year2019.intcode_computer.IntcodeComputer;
+
+import java.util.List;
+import java.util.Scanner;
+
+public class Day19 extends AdventDay {
+	
+	public static void main(String[] args) {
+		new DayRunner(new Day19()).runAll();
+	}
+	
+	@Override
+	public List<ExpectedAnswers> expected() {
+		return List.of(
+				new ExpectedAnswers("input.txt", 154, 9791328)
+		);
+	}
+	
+	IntcodeComputer computer;
+	
+	@Override
+	public void prepare(String file) {
+		Scanner input = Utils.scanFileNearClass(getClass(), file);
+		computer = IntcodeComputer.parse(input.nextLine());
+	}
+	
+	@Override
+	public Object part1() {
+		int width = 50;
+		long affectedPoints = 0;
+		for (int y = 0; y < width; y++)
+			for (int x = 0; x < width; x++)
+				affectedPoints += check(x, y);
+		return affectedPoints;
+	}
+	
+	@Override
+	public Object part2() {
+		int width = 100;
+		Point topLeft = Point.ZERO;
+		while (check(topLeft.x() + width - 1, topLeft.y()) <= 0) {
+			while (check(topLeft.x() + width - 1, topLeft.y()) <= 0)
+				topLeft = topLeft.shift(0, 1);
+			while (check(topLeft.x(), topLeft.y() + width - 1) <= 0)
+				topLeft = topLeft.shift(1, 0);
+		}
+		return topLeft.x() * 10000 + topLeft.y();
+	}
+	
+	long check(int x, int y) {
+		return computer.copy().runUntilOutput(InputProvider.constant(x, y));
+	}
+}
