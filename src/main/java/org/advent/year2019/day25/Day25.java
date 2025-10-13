78bc1cf01645fb4bdb4e648bd3fef22bc7f3472f
alaster <igor.aka92@gmail.com>
2025-10-13 23:47:42 +0300

    2019, day 25

--- src/main/java/org/advent/year2019/day25/Day25.java
+++ src/main/java/org/advent/year2019/day25/Day25.java
@@ -13,6 +13,7 @@ import org.advent.year2019.intcode_computer.OutputConsumer;
 import org.apache.commons.lang3.StringUtils;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.LinkedHashMap;
@@ -24,19 +25,19 @@ import java.util.Optional;
 import java.util.Queue;
 import java.util.Scanner;
 import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Day25 extends AdventDay {
 	
 	public static void main(String[] args) {
-//		new DayRunner(new Day25()).runAll();
-		new DayRunner(new Day25()).run("input.txt", 1);
+		new DayRunner(new Day25()).runAll();
 	}
 	
 	@Override
 	public List<ExpectedAnswers> expected() {
 		return List.of(
-//				new ExpectedAnswers("input2.txt", null, null)
-				new ExpectedAnswers("input.txt", null, null)
+				new ExpectedAnswers("input.txt", 1073815584, ExpectedAnswers.IGNORE)
 		);
 	}
 	
@@ -57,8 +58,13 @@ public class Day25 extends AdventDay {
 		InputProvider input = new WalkerInputProvider(outputBuffer);
 		
 		IntcodeComputer2 computer = new IntcodeComputer2(program, input, output);
-		
 		computer.run();
+		String finalOutput = outputBuffer.read();
+		
+		Matcher matcher = Pattern.compile("You should be able to get in by typing (\\d+) on the keypad at the main airlock")
+				.matcher(finalOutput);
+		if (matcher.find())
+			return Long.parseLong(matcher.group(1));
 		return null;
 	}
 	
@@ -114,8 +120,7 @@ public class Day25 extends AdventDay {
 	private static class WalkerInputProvider implements InputProvider {
 		final OutputConsumer.BufferingTextOutputConsumer outputBuffer;
 		InputProvider delegate = InputProvider.empty();
-		InputProvider console = InputProvider.console();
-		boolean manual = false;
+		Macro macro;
 		
 		@Override
 		public boolean hasNext() {
@@ -124,20 +129,43 @@ public class Day25 extends AdventDay {
 		
 		@Override
 		public long nextInput() {
-			if (manual)
-				return console.nextInput();
-			
 			if (!delegate.hasNext()) {
-				String nextCommand = nextCommands(outputBuffer.read());
-				if (nextCommand == null) {
-					manual = true;
-					System.out.println("\nMANUAL!");
-					return console.nextInput();
-				}
-				System.out.println("<< " + nextCommand);
-				delegate = InputProvider.ascii(nextCommand + "\n");
+				String nextInput = nextInput(outputBuffer.read());
+				System.out.println("<< " + nextInput);
+				delegate = InputProvider.ascii(nextInput + "\n");
 			}
 			return delegate.nextInput();
+		}
+		
+		public String nextInput(String output) {
+			while (true) {
+				if (macro != null) {
+					String nextCommand = macro.nextCommands(output);
+					if (nextCommand != null)
+						return nextCommand;
+					
+					System.out.println("MACRO FINISHED");
+					macro = null;
+					continue;
+				}
+				
+				String manualInput = new Scanner(System.in).nextLine();
+				
+				if (manualInput.startsWith("macro ")) {
+					String[] split = manualInput.split(" ");
+					macro = switch (split[1]) {
+						case "inspect" -> new InspectMacro();
+						case "goto" -> new GotoMacro(StringUtils.removeStart(manualInput, "macro goto").trim());
+						case "break" -> new BruteForceMacro();
+						default -> throw new RuntimeException("Unknown macro: " + manualInput);
+					};
+					continue;
+				}
+				
+				if (manualInput.equals("exit"))
+					throw new RuntimeException("Exit");
+				return manualInput;
+			}
 		}
 		
 		final Set<String> ignoredItems = Set.of("escape pod", "infinite loop", "molten lava",
@@ -149,67 +177,171 @@ public class Day25 extends AdventDay {
 		StringBuilder currentPath = new StringBuilder();
 		Queue<Action> actionsQueue = new LinkedList<>();
 		
+		interface Macro {
+			String nextCommands(String output);
+		}
 		
-		String nextCommands(String output) {
-			System.out.println("CURRENT PATH: " + currentPath);
-			if (!actionsQueue.isEmpty())
-				return actionsQueue.poll().run();
+		class InspectMacro implements Macro {
 			
-			Room room = rooms.get(currentPath.toString());
-//			System.out.println(room);
+			@Override
+			public String nextCommands(String output) {
+				System.out.println("CURRENT PATH: " + currentPath);
+				if (!actionsQueue.isEmpty())
+					return actionsQueue.poll().run();
+				
+				Room room = rooms.get(currentPath.toString());
+//			    System.out.println(room);
+				
+				if (room == null) {
+					room = Room.parse(output);
+					if (roomsByName.containsKey(room.name)) {
+						room = roomsByName.get(room.name);
+					} else {
+						System.out.println("NEW ROOM: " + rooms.size());
+						rooms.put(currentPath.toString(), room);
+						roomsByName.put(room.name, room);
+						
+						if (room.items.stream().anyMatch(i -> !ignoredItems.contains(i))) {
+							room.items.stream()
+									.filter(i -> !ignoredItems.contains(i))
+									.map(i -> new Action("take " + i, () -> {
+									}))
+									.forEach(actionsQueue::add);
+							return Objects.requireNonNull(actionsQueue.poll()).run();
+						}
+					}
+				}
+				
+				Direction back = currentPath.isEmpty() ? null : Direction.parseCompassLetter(currentPath.charAt(currentPath.length() - 1)).reverse();
+				
+				Room _room = room;
+				Optional<Direction> nextDirection = room.doors.stream()
+						.filter(d -> d != back)
+						.filter(d -> !checkedDirections.contains(Pair.of(_room.name, d)))
+//				    	.filter(d -> !rooms.containsKey(currentPath.toString() + d.compassLetter()))
+						.findAny();
+				if (nextDirection.isPresent() && !room.name.equals("Security Checkpoint")) {
+					Direction direction = nextDirection.get();
+					currentPath.append(direction.compassLetter());
+					checkedDirections.add(Pair.of(_room.name, direction));
+					return direction.getCompassName();
+				}
+				
+				if (back != null) {
+					currentPath.setLength(currentPath.length() - 1);
+					return back.getCompassName();
+				}
+
+//		    	System.out.println("All rooms:");
+//		    	rooms.values().forEach(System.out::println);
+
+//			    throw new RuntimeException("Not implemented yet");
+				return null;
+			}
+		}
+		
+		class GotoMacro implements Macro {
+			Queue<Integer> directions = new LinkedList<>();
 			
-			if (room == null) {
-				room = Room.parse(output);
-				if (roomsByName.containsKey(room.name)) {
-					room = roomsByName.get(room.name);
-				} else {
-					System.out.println("NEW ROOM: " + rooms.size());
-					rooms.put(currentPath.toString(), room);
-					roomsByName.put(room.name, room);
+			// TODO идет только с начала
+			public GotoMacro(String targetRoom) {
+				if (targetRoom.isEmpty())
+					targetRoom = "Security Checkpoint";
+				String finalTargetRoom = targetRoom;
+				rooms.entrySet().stream()
+						.filter(e -> e.getValue().name.equals(finalTargetRoom))
+						.map(Map.Entry::getKey)
+						.flatMapToInt(String::chars)
+						.forEach(directions::add);
+			}
+			
+			@Override
+			public String nextCommands(String output) {
+				if (directions.isEmpty())
+					return null;
+				Direction direction = Direction.parseCompassLetter((char) (int) directions.poll());
+				currentPath.append(direction.compassLetter());
+				return direction.getCompassName();
+			}
+		}
+		
+		class BruteForceMacro implements Macro {
+			static final int STATE_NONE = 0;
+			static final int STATE_PICKING = 1;
+			static final int STATE_WALKING = 2;
+			int state = STATE_NONE;
+			
+			Direction targetDirection;
+			List<String> allItems;
+			Set<String> currentItems;
+			
+			int variant = 0;
+			Set<String> currentAttemptItems;
+			
+			// TODO нужно проверять где сейчас находится
+			
+			@Override
+			public String nextCommands(String output) {
+				if (targetDirection == null) {
+					Direction back = Direction.parseCompassLetter(currentPath.charAt(currentPath.length() - 1)).reverse();
+					targetDirection = rooms.get(currentPath.toString()).doors.stream()
+							.filter(d -> d != back)
+							.findAny()
+							.orElseThrow();
+				}
+				if (allItems == null) {
+					if (!output.contains("Items in your inventory"))
+						return "inv";
+					allItems = Arrays.stream(output.split("\n"))
+							.filter(s -> s.startsWith("- "))
+							.map(s -> s.substring(2))
+							.toList();
+					currentItems = new HashSet<>(allItems);
+				}
+				
+				while (true) {
+					if (state == STATE_PICKING) {
+						for (String item : allItems) {
+							if (currentItems.contains(item) && !currentAttemptItems.contains(item)) {
+								currentItems.remove(item);
+								return "drop " + item;
+							}
+							if (!currentItems.contains(item) && currentAttemptItems.contains(item)) {
+								currentItems.add(item);
+								return "take " + item;
+							}
+						}
+						currentAttemptItems = null;
+						state = STATE_WALKING;
+					}
 					
-					if (room.items.stream().anyMatch(i -> !ignoredItems.contains(i))) {
-						room.items.stream()
-								.filter(i -> !ignoredItems.contains(i))
-								.map(i -> new Action("take " + i, () -> {
-								}))
-								.forEach(actionsQueue::add);
-						return Objects.requireNonNull(actionsQueue.poll()).run();
+					if (state == STATE_WALKING) {
+						if (!output.contains("you are ejected back to the checkpoint"))
+							return targetDirection.getCompassName();
+						state = STATE_NONE;
+					}
+					
+					if (currentAttemptItems == null) {
+						variant++;
+						if (variant > 1 << allItems.size())
+							return null;
+						
+						currentAttemptItems = itemsVariant(variant);
+						state = STATE_PICKING;
 					}
 				}
 			}
 			
-			Direction back = currentPath.isEmpty() ? null : Direction.parseCompassLetter(currentPath.charAt(currentPath.length() - 1)).reverse();
-			
-			Room _room = room;
-			Optional<Direction> nextDirection = room.doors.stream()
-					.filter(d -> d != back)
-					.filter(d -> !checkedDirections.contains(Pair.of(_room.name, d)))
-//					.filter(d -> !rooms.containsKey(currentPath.toString() + d.compassLetter()))
-					.findAny();
-			if (nextDirection.isPresent()) {
-				Direction direction = nextDirection.get();
-				currentPath.append(direction.compassLetter());
-				checkedDirections.add(Pair.of(_room.name, direction));
-				return direction.getCompassName();
+			Set<String> itemsVariant(int variant) {
+				Set<String> itemsVariant = new HashSet<>();
+				int index = 0;
+				for (String item : allItems) {
+					if ((variant & 1 << index) > 0)
+						itemsVariant.add(item);
+					index++;
+				}
+				return itemsVariant;
 			}
-			
-			if (back != null) {
-				currentPath.setLength(currentPath.length() - 1);
-				return back.getCompassName();
-			}
-			
-//			System.out.println("All rooms:");
-//			rooms.values().forEach(System.out::println);
-
-//			throw new RuntimeException("Not implemented yet");
-			return null;
-		}
-	}
-	
-	static class DirectionsMaze<T> {
-		static class Node<T> {
-			T value;
-			Map<Direction, Node<T>> neighbors = new LinkedHashMap<>();
 		}
 	}
 }
