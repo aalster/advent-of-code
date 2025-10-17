0008d561c1755c2cba9c5b5713683cd7b0217706
alaster <igor.aka92@gmail.com>
2025-10-18 00:13:36 +0300

    refactoring

--- src/main/java/org/advent/year2021/day15/Day15.java
+++ src/main/java/org/advent/year2021/day15/Day15.java
@@ -30,62 +30,63 @@ public class Day15 extends AdventDay {
 		);
 	}
 	
-	Map<Point, Integer> field;
+	int[][] dangerField;
 	
 	@Override
 	public void prepare(String file) {
 		Scanner input = Utils.scanFileNearClass(getClass(), file);
-		field = Point.readFieldMap(Utils.readLines(input)).entrySet().stream()
+		Map<Point, Integer> field = Point.readFieldMap(Utils.readLines(input)).entrySet().stream()
 				.collect(Collectors.toMap(Map.Entry::getKey, e -> e.getValue() - '0'));
+		Point end = Point.maxBound(field.keySet());
+		dangerField = new int[end.x() + 1][end.y() + 1];
+		field.forEach((p, d) -> dangerField[p.y()][p.x()] = d);
 	}
 	
 	@Override
 	public Object part1() {
-		return solve(field);
+		return solve(dangerField);
 	}
 	
 	@Override
 	public Object part2() {
-		int width = field.keySet().stream().mapToInt(Point::x).max().orElse(0) + 1;
-		int height = field.keySet().stream().mapToInt(Point::y).max().orElse(0) + 1;
+		int height = dangerField.length;
+		int width = dangerField[0].length;
+		int[][] extendedField = new int[height * 5][width * 5];
 		
-		Map<Point, Integer> extendedField = new HashMap<>();
-		for (int y = 0; y < 5; y++) {
-			int _y = y;
-			int dy = y * height;
-			for (int x = 0; x < 5; x++) {
-				int _x = x;
-				int dx = x * width;
-				extendedField.putAll(field.entrySet().stream().collect(Collectors.toMap(
-						e -> e.getKey().shift(dx, dy),
-						e -> wrapDangerLevel(e.getValue() + _x + _y)
-				)));
+		for (int chunkY = 0; chunkY < 5; chunkY++) {
+			int dy = chunkY * height;
+			for (int chunkX = 0; chunkX < 5; chunkX++) {
+				int dx = chunkX * width;
+				
+				for (int y = 0; y < height; y++) {
+					for (int x = 0; x < width; x++) {
+						int danger = dangerField[y][x] + chunkX + chunkY;
+						extendedField[y + dy][x + dx] = danger <= 9 ? danger : danger - 9;
+					}
+				}
 			}
 		}
-		
 		return solve(extendedField);
 	}
 	
-	int wrapDangerLevel(int sum) {
-		return sum <= 9 ? sum : sum - 9;
-	}
-	
-	long solve(Map<Point, Integer> dangerField) {
-		Map<Point, Long> minTotalDanger = new HashMap<>();
+	int solve(int[][] dangerField) {
+		Point end = new Point(dangerField.length - 1, dangerField[0].length - 1);
+		Map<Point, Integer> minTotalDanger = new HashMap<>(dangerField.length * dangerField[0].length * 2);
 		
-		Queue<Path> paths = new PriorityQueue<>(Comparator.comparing(Path::danger));
+		Queue<Path> paths = new PriorityQueue<>(1000, Comparator.comparing(Path::danger));
 		paths.add(new Path(Point.ZERO, 0));
 		
 		while (!paths.isEmpty()) {
 			Path path = paths.poll();
-			for (Direction d : Direction.values()) {
+			for (Direction d : Direction.VALUES) {
 				Point nextPosition = d.shift(path.position);
-				Integer danger = dangerField.get(nextPosition);
-				if (danger == null)
+				if (nextPosition.x() < 0 || nextPosition.y() < 0
+						|| nextPosition.y() >= dangerField.length || nextPosition.x() >= dangerField[0].length)
 					continue;
+				int danger = dangerField[nextPosition.x()][nextPosition.y()];
 				
-				long nextDanger = path.danger + danger;
-				Long minDanger = minTotalDanger.get(nextPosition);
+				int nextDanger = path.danger + danger;
+				Integer minDanger = minTotalDanger.get(nextPosition);
 				if (minDanger != null && minDanger <= nextDanger)
 					continue;
 				
@@ -93,11 +94,9 @@ public class Day15 extends AdventDay {
 				paths.add(new Path(nextPosition, nextDanger));
 			}
 		}
-		
-		Point end = Point.maxBound(dangerField.keySet());
-		return minTotalDanger.getOrDefault(end, -1L);
+		return minTotalDanger.getOrDefault(end, 0);
 	}
 	
-	record Path(Point position, long danger) {
+	record Path(Point position, int danger) {
 	}
 }
